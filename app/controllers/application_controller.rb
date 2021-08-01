ada98adc0e0e160c022be4ff5e08907b7583cdac
daisuke.nagamatsu <mugiblog5656@gmail.com>
2021-08-01 04:54:01 +0000

    Add One-to-many

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-before_action :require_user_logged_in, only: [:index, :show]
-     
     
      include SessionsHelper 
      include Pagy::Backend 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in, only: [:index, :show]
+    before_action :correct_user, only: [:show,:destroy]
     def index
-        @pagy, @tasks = pagy(Task.all)
+        @task = current_user.tasks.build #form_with 用
+        @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
     end
     
     def show
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find(params[:id])
     end
     
     def new
@@ -13,7 +15,7 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = 'Task が正常に投稿されました'
@@ -25,11 +27,11 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find_by(params[:id])
     end
     
     def update
-        @task = Task.find(params[:id])
+        @task = Task.find_by(params[:id])
         
         if @task.update(task_params)
             flash[:success] = 'Task は正常に更新されました'
@@ -41,11 +43,9 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
-        
         flash[:success] = 'Task は正常に削除されました'
-        redirect_to tasks_url
+        redirect_back(fallback_location: root_path)
     end
     
     private
@@ -53,5 +53,12 @@ class TasksController < ApplicationController
 # Strong Parameter
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+          redirect_to root_url
+        end
     end
 end
